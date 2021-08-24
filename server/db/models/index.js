041c57550a1fce37f4bc89687db4847bdc26b7b6
IbrahimMohammed47 <ibrahim.mohammed.4748@gmail.com>
2021-08-25 01:09:48 +0200

    added group model and some associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,14 +1,28 @@
 const Conversation = require("./conversation");
 const User = require("./user");
 const Message = require("./message");
-
+const Group = require("./group");
 // associations
 
 User.hasMany(Conversation);
 Conversation.belongsTo(User, { as: "user1" });
 Conversation.belongsTo(User, { as: "user2" });
+
+// 1 conversation : M msgs relation
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
+
+// 1 admin : M groups relation
+Group.belongsTo(User, { foreignKey: 'admin'});
+User.hasMany(Group, { foreignKey: 'admin'});
+
+// 1 group : M msgs relation
+Message.belongsTo(Group, { foreignKey: 'groupId'});
+Group.hasMany(Message, { foreignKey: 'groupId'});
+
+// M participants : N groups relation (junction table)
+User.belongsToMany(Group, { through: 'groups_participants' });
+Group.belongsToMany(User, { through: 'groups_participants' });
 
 module.exports = {
   User,
